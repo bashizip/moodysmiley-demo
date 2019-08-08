bb07202e24346d0ceb82474b5b923cc324791614
Patrick Bashizi <bashizip@gmail.com>
2019-08-08 11:28:25 +0300

    Initial commit

---
+++ lib/src/main/java/com/bashizip/moodysmiley/MoodySmileyView.kt
@@ -0,0 +1,170 @@
+package com.bashizip.moodysmiley
+
+import android.content.Context
+import android.graphics.*
+import android.os.Handler
+import android.util.AttributeSet
+import android.view.View
+import com.bashizip.moodysmiley.R
+
+
+class SmileyView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+
+    private val borderwidth = 4.0f
+
+    private var faceColor = DEFAULT_FACE_COLOR
+    private var eyesColor = DEFAULT_EYES_COLOR
+    private var mouthColor = DEFAULT_MOUTH_COLOR
+    private var borderColor = DEFAULT_BORDER_COLOR
+    private var borderWidth = DEFAULT_BORDER_WIDTH
+
+    private val paint = Paint()
+    private val mouthPath = Path()
+    private var size = 320
+
+    private  var mHandler = Handler()
+
+    var moody = true
+
+    init {
+
+        paint.isAntiAlias = true
+        setupAttributes(attrs)
+
+        setOnClickListener {
+            switchState()
+        }
+
+    }
+
+    fun switchState(){
+        if (happinessState == HAPPY) {
+            happinessState = SAD
+        } else {
+            happinessState = HAPPY
+        }
+    }
+
+
+    var happinessState = HAPPY
+        set(state) {
+            field = state
+            invalidate()
+        }
+
+
+    private fun setupAttributes(attrs: AttributeSet?) {
+        // 6
+        // Obtain a typed array of attributes
+        val typedArray = context.theme.obtainStyledAttributes(
+            attrs, R.styleable.SmileyView,
+            0, 0
+        )
+
+        // 7
+        // Extract custom attributes into member variables
+        happinessState = typedArray.getInt(R.styleable.SmileyView_state, HAPPY.toInt()).toLong()
+        faceColor = typedArray.getColor(R.styleable.SmileyView_faceColor, DEFAULT_FACE_COLOR)
+        eyesColor = typedArray.getColor(R.styleable.SmileyView_eyesColor, DEFAULT_EYES_COLOR)
+        mouthColor = typedArray.getColor(R.styleable.SmileyView_mouthColor, DEFAULT_MOUTH_COLOR)
+        borderColor = typedArray.getColor(
+            R.styleable.SmileyView_borderColor,
+            DEFAULT_BORDER_COLOR
+        )
+        borderWidth = typedArray.getDimension(
+            R.styleable.SmileyView_borderWidth,
+            DEFAULT_BORDER_WIDTH
+        )
+
+        // 8
+        // TypedArray objects are shared and must be recycled.
+        typedArray.recycle()
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        drawFaceBackground(canvas)
+        drawEyes(canvas)
+        drawMouth(canvas)
+
+        if (moody) {
+            handler.postDelayed({ switchState() }, 2000)
+        }
+    }
+
+
+    private fun drawFaceBackground(canvas: Canvas?) {
+
+        paint.color = faceColor
+        paint.style = Paint.Style.FILL
+
+        val radius = size / 2f
+
+        canvas?.drawCircle(size / 2f, size / 2f, radius, paint)
+
+        paint.color = borderColor
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = borderwidth
+
+        canvas?.drawCircle(size / 2f, size / 2f, radius - borderwidth, paint)
+
+    }
+
+
+    private fun drawEyes(canvas: Canvas?) {
+        // 1
+        paint.color = eyesColor
+        paint.style = Paint.Style.FILL
+
+        val leftEyeRect = RectF(size * 0.32f, size * 0.23f, size * 0.43f, size * 0.50f)
+
+        canvas?.drawOval(leftEyeRect, paint)
+        val rightEyeRect = RectF(size * 0.57f, size * 0.23f, size * 0.68f, size * 0.50f)
+
+        canvas?.drawOval(rightEyeRect, paint)
+    }
+
+
+    private fun drawMouth(canvas: Canvas?) {
+
+        // Clear
+        mouthPath.reset()
+
+        mouthPath.moveTo(size * 0.22f, size * 0.7f)
+
+        if (happinessState == HAPPY) {
+            // Happy mouth path
+            mouthPath.quadTo(size * 0.5f, size * 0.80f, size * 0.78f, size * 0.7f)
+            mouthPath.quadTo(size * 0.5f, size * 0.90f, size * 0.22f, size * 0.7f)
+        } else {
+            // Sad mouth path
+            mouthPath.quadTo(size * 0.5f, size * 0.50f, size * 0.78f, size * 0.7f)
+            mouthPath.quadTo(size * 0.5f, size * 0.60f, size * 0.22f, size * 0.7f)
+        }
+
+        paint.color = mouthColor
+        paint.style = Paint.Style.FILL
+
+        // Draw mouth path
+        canvas?.drawPath(mouthPath, paint)
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        size = Math.min(measuredWidth, measuredHeight)
+        setMeasuredDimension(size, size)
+    }
+
+    companion object {
+
+        private const val DEFAULT_FACE_COLOR = Color.YELLOW
+        private const val DEFAULT_EYES_COLOR = Color.BLACK
+        private const val DEFAULT_MOUTH_COLOR = Color.BLACK
+        private const val DEFAULT_BORDER_COLOR = Color.BLACK
+        private const val DEFAULT_BORDER_WIDTH = 4.0f
+
+        const val HAPPY = 0L
+        const val SAD = 1L
+    }
+}
